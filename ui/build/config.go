1fea396d2a60deb57f2b88079de2dd86a0233113
Nick Reuter <nreuter85@gmail.com>
2022-11-07 17:15:03 -0600

    soong: set KERNEL_LTO to thin at runtime if total ram < 64GB
    Change-Id: I4b6ab0b20a55949348ce8efca1e35a5156b94d83

--- ui/build/config.go
+++ ui/build/config.go
@@ -455,6 +455,18 @@ func NewConfig(ctx Context, args ...string) Config {
 		ret.riggedDistDirForBazel = ret.distDir
 	}
 
+	kernelLtoFlag := ""
+	if buildFlag, ok := ret.environ.Get("KERNEL_LTO"); ok {
+		kernelLtoFlag = buildFlag
+	} else if ret.totalRAM <= 64*1024*1024*1024 {
+		// Less than 64GB of ram, restrict to thin LTO compilation
+		kernelLtoFlag = "thin"
+	}
+
+	if kernelLtoFlag != "" {
+		ret.environ.Set("KERNEL_LTO", kernelLtoFlag)
+	}
+
 	c := Config{ret}
 	storeConfigMetrics(ctx, c)
 	return c
